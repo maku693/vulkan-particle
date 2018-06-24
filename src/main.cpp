126b3b1f5d760c84c6738282ee232df2eb47fa6c
Shota HAMADA <maku3ts@gmail.com>
2018-06-24 23:21:01 +0900

    Show a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
-#include <Windows.h>
+#include "win32.h"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int) {}
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int) {
+  const auto hwnd = win32::create_window();
+  while (win32::process_messages()) {
+  }
+}
---
+++ src/win32.h
@@ -0,0 +1,51 @@
+#include <Windows.h>
+
+namespace win32 {
+
+namespace detail {
+
+static LRESULT CALLBACK wndproc(HWND hwnd, UINT msg, WPARAM wparam,
+                                LPARAM lparam) {
+  if (msg == WM_DESTROY) {
+    PostQuitMessage(0);
+    return 0;
+  }
+
+  return DefWindowProcW(hwnd, msg, wparam, lparam);
+}
+
+static constexpr auto window_class_name = L"main";
+
+static void register_window_class() {
+  WNDCLASSEXW wcex{};
+  wcex.cbSize = sizeof(WNDCLASSEXW);
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = wndproc;
+  wcex.hInstance = ::GetModuleHandleW(nullptr);
+  wcex.hbrBackground = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
+  wcex.lpszClassName = window_class_name;
+  ::RegisterClassExW(&wcex);
+}
+
+} // namespace detail
+
+static HWND create_window() {
+  detail::register_window_class();
+  return CreateWindowExW(0, detail::window_class_name, L"", WS_OVERLAPPEDWINDOW,
+                  CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, nullptr, nullptr,
+                  ::GetModuleHandleW(nullptr), nullptr);
+}
+
+static bool process_messages() {
+  MSG msg{};
+  while (::PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE)) {
+    ::TranslateMessage(&msg);
+    ::DispatchMessageW(&msg);
+    if (msg.message == WM_QUIT) {
+      return false;
+    }
+  }
+  return true;
+}
+
+} // namespace win32
